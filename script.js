d5d992c0a5d61196fde7839828d4ea08d1ce0080
shubhangiD <shubhangi.22010293@viit.ac.in>
2024-01-21 18:10:40 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const bar = document.querySelector('#bar');
+const nav = document.querySelector('#navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    });
+}
+
+if(close){
+    close.addEventListener('click',()=>{
+        nav.classList.remove('active')
+    })
+}
